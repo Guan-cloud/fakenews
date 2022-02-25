eea1ab563052f3c259933e13937001c7017c189e
Guan-cloud <zhengyi_guan@163.com>
2022-02-25 09:03:23 +0800

    Delete data/data_new directory

--- data/data_new/text.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df=pd.read_excel('test.xlsx')
-print(df)
-df.to_csv('test.csv',index=False,encoding='utf-8')
--- data/data_new/tolist.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-pf=pd.read_csv('xlm-4.csv')
-list=pf['label'].values.tolist()
-print(list)
